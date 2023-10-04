701c8f5332bab65c336d16e0401faa141cb7a069
Josehneves <josehneves28@gmail.com>
2023-10-04 16:02:31 -0400

    First commit, includes html, css and js

---
+++ app.js
@@ -0,0 +1,12 @@
+//level 1
+
+let score = 0;
+let firstSelection = "";
+let secondSelection = "";
+
+const grid1 = document.querySelectorAll(".grid1 .cards");
+grid1.forEach((cards) => {
+    cards.addEventListener("click", () => {
+       console.log(true);
+    });
+});
